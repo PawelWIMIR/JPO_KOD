73c3e4908be8f14cb01233ec713c51a266013084
PawelWIMIR <37414542+PawelWIMIR@users.noreply.github.com>
2018-04-12 23:27:05 +0200

    Add files via upload

--- JPO_3/src/com/company/SpringApplet.java
+++ JPO_3/src/com/company/SpringApplet.java
@@ -1,35 +1,40 @@
 package com.company;
 import javax.swing.*;
-import java.awt.*;
+
 //main class
 public class SpringApplet extends JApplet{
 
+    public void init() {
 
-    public static void main(String[] args) {
         //1
-        Vector2D vector1 = new Vector2D(4f,6f);
-        Vector2D vector2 = new Vector2D(3f,2f);
+        Vector2D vector1 = new Vector2D(5f,3f);
+        Vector2D vector2 = new Vector2D(4f,6f);
         //2
-        Vector2D sum = vector1.VectorSum(vector2);
-        Vector2D subs= vector1.VectorSubtraction(vector2);
+        Vector2D suma = vector1.VectorSumowanie(vector2);
+        Vector2D odjecie= vector1.VectorOdejmowanie(vector2);
         //3
-        Vector2D normalized1 = vector1.VectorNormalized();
-        Vector2D normalized2 = vector2.VectorNormalized();
+        Vector2D znormalizowany1 = vector1.VectorZnormalizownay();
+        Vector2D znormalizowany2 = vector2.VectorZnormalizownay();
         //4
-        float length1 = vector1.VectorLength();
-        float length2 = vector2.VectorLength();
+        float length1 = vector1.VectorDlugosc();
+        float length2 = vector2.VectorDlugosc();
         //5
-        Vector2D multiplication1 = vector1.VectorMultiplication(6f);
+        Vector2D przemnozenie = vector1.VectorMnozenie(5f);
         //6
-        System.out.println("vector1 coordinates (X: "+vector1.posX+", Y: "+vector1.posY+")");
-        System.out.println("vector2 coordinates (X: "+vector2.posX+", Y: "+vector2.posY+")");
-        System.out.println("sum coordinates (X: "+sum.posX+", Y: "+sum.posY+")");
-        System.out.println("subs coordinates (X: "+subs.posX+", Y: "+subs.posY+")");
-        System.out.println("normalized1 coordinates (X: "+normalized1.posX+", Y: "+normalized1.posY+")");
-        System.out.println("normalized2 coordinates (X: "+normalized2.posX+", Y: "+normalized2.posY+")");
-        System.out.println("multiplication1 coordinates (X: "+multiplication1.posX+", Y: "+multiplication1.posY+")");
-        System.out.println("length1: "+vector1.VectorLength());
-        System.out.println("length2: "+vector2.VectorLength());
+        System.out.println("vector1 wspolrzedne (X: "+vector1.posX+", Y: "+vector1.posY+")");
+        System.out.println("vector2 wspolrzedne (X: "+vector2.posX+", Y: "+vector2.posY+")");
+        System.out.println("sumowanie wspolrzednych (X: "+suma.posX+", Y: "+suma.posY+")");
+        System.out.println("odejmowanie wspolrzednych (X: "+odjecie.posX+", Y: "+odjecie.posY+")");
+        System.out.println("znormalizowany wektor1 (X: "+znormalizowany1.posX+", Y: "+znormalizowany1.posY+")");
+        System.out.println("znormalizowany wektor2 (X: "+znormalizowany2.posX+", Y: "+znormalizowany2.posY+")");
+        System.out.println("wymnozenie wektora1 przez liczbe (X: "+przemnozenie.posX+", Y: "+przemnozenie.posY+")");
+        System.out.println("dlugosc1: "+vector1.VectorDlugosc());
+        System.out.println("dlugosc: "+vector2.VectorDlugosc());
+
 
     }
+
+    public void paint() {
+    }
+
 }
--- JPO_3/src/com/company/Vector2D.java
+++ JPO_3/src/com/company/Vector2D.java
@@ -1,54 +1,53 @@
 package com.company;
 
 import static java.lang.Math.sqrt;
-
 public class Vector2D {
     //1
-    //vector coordinates
+    //vector współrzędne
     public float posX;
     public float posY;
     //2
-    //default constructor
+    //default konstrutkor
     public Vector2D()
     {
         this.posX = 0f;
         this.posY = 0f;
 
     }
-    //constructor with parameters
+    //konstruktor z parametrami
     public Vector2D(float posX, float posY)
     {
         this.posX = posX;
         this.posY = posY;
     }
     //3
-    public Vector2D VectorSum(Vector2D v)
+    public Vector2D VectorSumowanie(Vector2D v)
     {
 
         Vector2D vektor = new Vector2D(this.posX + v.posX, this.posY+v.posY);
         return vektor;
     }
     //4
-    public Vector2D VectorSubtraction(Vector2D v)
+    public Vector2D VectorOdejmowanie(Vector2D v)
     {
         Vector2D vektor = new Vector2D(this.posX-v.posX, this.posY-v.posY);
         return vektor;
     }
     //5
-    public Vector2D VectorMultiplication (float x)
+    public Vector2D VectorMnozenie (float x)
     {
         Vector2D vector = new Vector2D(this.posX * x, this.posY * x);
         return vector;
     }
     //6
-    public float VectorLength()
+    public float VectorDlugosc()
     {
         return (float)sqrt(this.posX*this.posX + this.posY*this.posY);
     }
     //7
-    public Vector2D VectorNormalized()
+    public Vector2D VectorZnormalizownay()
     {
-        Vector2D vector = new Vector2D(this.posX/VectorLength(), this.posY/VectorLength());
+        Vector2D vector = new Vector2D(this.posX/VectorDlugosc(), this.posY/VectorDlugosc());
         return vector;
     }
 }
